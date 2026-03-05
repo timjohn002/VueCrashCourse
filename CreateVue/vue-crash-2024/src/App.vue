d92c92c680665d161ad45dd9ac655bbd0912ff5d
Timothy <timjohnargota@gmail.com>
2026-03-05 17:21:44 +0800

    Added Hero.vue

--- CreateVue/vue-crash-2024/src/App.vue
+++ CreateVue/vue-crash-2024/src/App.vue
@@ -1,5 +1,10 @@
+<script setup>
+import Navbar from '@/components/Navbar.vue';
+import Hero from '@/components/Hero.vue';
+</script>
+
 
 <template>
-  <h1 class="text-2xl"> Vue Jobs</h1>
-  
+  <Navbar />
+  <Hero title="This is the title" />
 </template>
