09e4ec540d4fe9db2f65ab6b0eccad35ffa44ef6
Timothy <timjohnargota@gmail.com>
2026-03-06 11:34:59 +0800

    Added dynamic background color property to cards.

--- CreateVue/vue-crash-2024/src/components/Card.vue
+++ CreateVue/vue-crash-2024/src/components/Card.vue
@@ -1,9 +1,18 @@
 <script setup>
+import {defineProps} from 'vue';
+
+defineProps({
+    bg: {
+        type: String,
+        default: 'bg-gray-100'
+    }
+})
 
 </script>
 
 <template>
-<div class="bg-gray-100 p-6 rounded-lg shadow-md">
+<div :class="`${bg} p-6 rounded-lg shadow-md`">
+    <slot></slot>
     
 </div>
 </template>
--- CreateVue/vue-crash-2024/src/components/HomeCards.vue
+++ CreateVue/vue-crash-2024/src/components/HomeCards.vue
@@ -1,4 +1,7 @@
-<script setup></script>
+<script setup>
+import Card from '@/components/Card.vue';
+
+</script>
 
 
 
@@ -6,7 +9,7 @@
     <section class="py-4">
       <div class="container-xl lg:container m-auto">
         <div class="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
-          <div class="bg-gray-100 p-6 rounded-lg shadow-md">
+          <Card>
             <h2 class="text-2xl font-bold">For Developers</h2>
             <p class="mt-2 mb-4">
               Browse our Vue jobs and start your career today
@@ -17,8 +20,8 @@
             >
               Browse Jobs
             </a>
-          </div>
-          <div class="bg-green-100 p-6 rounded-lg shadow-md">
+          </Card>
+          <Card bg="bg-red-100">
             <h2 class="text-2xl font-bold">For Employers</h2>
             <p class="mt-2 mb-4">
               List your job to find the perfect developer for the role
@@ -29,7 +32,7 @@
             >
               Add Job
             </a>
-          </div>
+          </Card>
         </div>
       </div>
     </section>
