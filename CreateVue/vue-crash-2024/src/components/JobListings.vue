4021c40cb419576b5a5d1c5783bb05e69eb0042d
Timothy <timjohnargota@gmail.com>
2026-03-06 15:03:13 +0800

    Showed example of using reactive() instead of ref().

--- CreateVue/vue-crash-2024/src/components/JobListings.vue
+++ CreateVue/vue-crash-2024/src/components/JobListings.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {ref, defineProps, onMounted} from 'vue';
+import {reactive, defineProps, onMounted} from 'vue';
 import JobListingSingular from '@/components/JobListingSingular.vue';
 import { RouterLink } from 'vue-router';
 import axios, { Axios } from 'axios';
@@ -14,16 +14,35 @@ const props = defineProps({
     }
 })
 
-const jobs = ref([]);
+// const jobs = ref([]);
+//Changed ref to reactive.
+/**
+ *  Key differences:
+ * 1. reactive() only takes objects and not primitives,
+ *    while ref() can
+ * 2. ref() has a .value property for reassigning.
+ *    reactive() doesn't have .value.
+ * 
+ */
+
+ const state = reactive({
+    jobs: [],
+    isLoading: true
+ });
 
 
 onMounted(async () => {
     try{
         const res = await axios.get('http://localhost:5000/jobs');
-        jobs.value = res.data;
+        state.jobs = res.data;
 
     }catch(error){
         console.error("Error fetching jobs.", error);
+    }finally {
+        state.isLoading = false;
+        // This assigning is okay
+
+        // state = ??? is not okay
     }
 });
 
@@ -37,13 +56,13 @@ onMounted(async () => {
                 Browse Jobs
              </h2>
              <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-            <JobListingSingular v-for="job in jobs.slice(0, maxRecords || jobs.length)" :key="job.id" :job="job"/>            
+            <JobListingSingular v-for="job in state.jobs.slice(0, maxRecords || state.jobs.length)" :key="job.id" :job="job"/>            
         </div>
         </div>
     </section>
 
 
-    <section v-if="showMoreBtn===true && jobs.length > maxRecords" class="m-auto max-w-lg my-10 px-6">
+    <section v-if="showMoreBtn===true && state.jobs.length > maxRecords" class="m-auto max-w-lg my-10 px-6">
       <RouterLink
         to="/jobs"
         class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
